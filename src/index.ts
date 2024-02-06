7d20a1bafcdb30677ca36fb15755f60d6a47d812
= <pamodhamahagamage98@gmail.com>
2024-02-06 13:25:49 +0530

    Feat-Add-Vecel Config fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+/*
+ * Load up and parse configuration details from
+ * the `.env` file to the `process.env`
+ * object of Node.js
+ */
 dotenv.config();
+
+/*
+ * Create an Express application and get the
+ * value of the PORT environment variable
+ * from the `process.env`
+ */
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
