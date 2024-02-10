2a6fc45b3eec31e5a4b0a2da946969879fd4ddba
= <pamodhamahagamage98@gmail.com>
2024-02-10 10:42:22 +0530

    Feat_Add-git ignore update

---
+++ dist/controllers/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// import userController from './user.controller';
+// import authController from './auth.controller';
+// import roleController from './role.controller';
+// export { userController, authController, roleController };
+//# sourceMappingURL=index.js.map
